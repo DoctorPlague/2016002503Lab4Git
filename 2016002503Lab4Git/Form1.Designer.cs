1077a3390288649def4fcebff0f23a2eb2be20e9
DoctorPlague <jjlyonsschool@gmail.com>
2016-09-04 10:45:15 +1200

    update

--- 2016002503Lab4Git/Form1.Designer.cs
+++ 2016002503Lab4Git/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taskListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forLoopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nestedForLoopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.whileLoopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doWhileLoopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forEachLoopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblLab4 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.taskListToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(233, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // taskListToolStripMenuItem
+            // 
+            this.taskListToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.forLoopToolStripMenuItem,
+            this.nestedForLoopToolStripMenuItem,
+            this.whileLoopToolStripMenuItem,
+            this.doWhileLoopToolStripMenuItem,
+            this.forEachLoopToolStripMenuItem});
+            this.taskListToolStripMenuItem.Name = "taskListToolStripMenuItem";
+            this.taskListToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.taskListToolStripMenuItem.Text = "Task List";
+            // 
+            // forLoopToolStripMenuItem
+            // 
+            this.forLoopToolStripMenuItem.Name = "forLoopToolStripMenuItem";
+            this.forLoopToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.forLoopToolStripMenuItem.Text = "For Loop";
+            this.forLoopToolStripMenuItem.Click += new System.EventHandler(this.forLoopToolStripMenuItem_Click);
+            // 
+            // nestedForLoopToolStripMenuItem
+            // 
+            this.nestedForLoopToolStripMenuItem.Name = "nestedForLoopToolStripMenuItem";
+            this.nestedForLoopToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.nestedForLoopToolStripMenuItem.Text = "Nested For Loop";
+            this.nestedForLoopToolStripMenuItem.Click += new System.EventHandler(this.nestedForLoopToolStripMenuItem_Click);
+            // 
+            // whileLoopToolStripMenuItem
+            // 
+            this.whileLoopToolStripMenuItem.Name = "whileLoopToolStripMenuItem";
+            this.whileLoopToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.whileLoopToolStripMenuItem.Text = "While Loop";
+            this.whileLoopToolStripMenuItem.Click += new System.EventHandler(this.whileLoopToolStripMenuItem_Click);
+            // 
+            // doWhileLoopToolStripMenuItem
+            // 
+            this.doWhileLoopToolStripMenuItem.Name = "doWhileLoopToolStripMenuItem";
+            this.doWhileLoopToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.doWhileLoopToolStripMenuItem.Text = "Do While Loop";
+            this.doWhileLoopToolStripMenuItem.Click += new System.EventHandler(this.doWhileLoopToolStripMenuItem_Click);
+            // 
+            // forEachLoopToolStripMenuItem
+            // 
+            this.forEachLoopToolStripMenuItem.Name = "forEachLoopToolStripMenuItem";
+            this.forEachLoopToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.forEachLoopToolStripMenuItem.Text = "For Each Loop";
+            this.forEachLoopToolStripMenuItem.Click += new System.EventHandler(this.forEachLoopToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblLab4
+            // 
+            this.lblLab4.AutoSize = true;
+            this.lblLab4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLab4.Location = new System.Drawing.Point(8, 34);
+            this.lblLab4.Name = "lblLab4";
+            this.lblLab4.Size = new System.Drawing.Size(218, 24);
+            this.lblLab4.TabIndex = 1;
+            this.lblLab4.Text = "Lab 4 Projects - Loops";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(16, 76);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(200, 200);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 293);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblLab4);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taskListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forLoopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nestedForLoopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem whileLoopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doWhileLoopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forEachLoopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label lblLab4;
+        private System.Windows.Forms.TextBox txtOutput;
     }
 }
 
--- 2016002503Lab4Git/Form1.cs
+++ 2016002503Lab4Git/Form1.cs
@@ -16,5 +16,79 @@ namespace _2016002503Lab4Git
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void forLoopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();   // Clears the text box so you can run each loop one after another and not have problems.
+            for(int num = 1; num <= 7; num++)
+            {
+                txtOutput.Text += num + "\r\n";
+            }
+        }
+
+        private void nestedForLoopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            for (int nest = 1; nest <= 7; nest++)
+            {
+                for(int nest2 = 1; nest2 <= nest; nest2++)
+                {
+                    txtOutput.Text += nest2;
+                }
+                txtOutput.Text += "\r\n";
+            }
+        }
+
+        private void whileLoopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            int wLoop = 0;  // W stands for While
+            while (wLoop <= 6)
+            {
+                wLoop++;
+                txtOutput.Text += wLoop + "\r\n";
+            }
+        }
+
+        private void doWhileLoopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            int dwLoop = 0;  // DW stands for DoWhile
+
+            do
+            {
+                dwLoop++;
+                txtOutput.Text += dwLoop + "\r\n";
+            }
+            while (dwLoop <= 6);
+        }
+
+        private void forEachLoopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            int[] feLoop = new int[] { 1, 2, 3, 4, 5, 6, 7};     // FE stands for ForEach       
+            foreach (int element in feLoop)
+            {
+                txtOutput.Text += element;
+                txtOutput.Text += "\r\n";
+            }
+                
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Help N/A");
+        }
     }
 }
